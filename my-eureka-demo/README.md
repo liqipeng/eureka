e76d551f48d77f67d2bd854186813ca81344dc58
liqipeng <liqipeng127@gmail.com>
2022-03-28 01:24:55 +0800

    eureka demo

---
+++ my-eureka-demo/README.md
@@ -0,0 +1,5 @@
+示例需在本地配置hosts
+```
+127.0.0.1 eureka-server1
+127.0.0.1 eureka-server2
+```
--- my-eureka-demo/eureka-client-product-service/src/main/java/com/example/eurekaclientdemo/ProductController.java
+++ my-eureka-demo/eureka-client-product-service/src/main/java/com/example/eurekaclientdemo/ProductController.java
@@ -2,7 +2,12 @@ package com.example.eurekaclientdemo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
@@ -14,5 +19,15 @@ public class ProductController {
         logger.info("product service invoked! name={}", name);
 
         return "product: " + name;
+    }
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        // from : https://spring.io/guides/gs/service-registration-and-discovery/
+        return this.discoveryClient.getInstances(applicationName);
     }
 }
